0fc1a65201c3bd5c38360a6e924a49108bd045c0
Shrinath Adhav <shrinathadhav45@gmail.com>
2025-05-17 09:20:56 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -166,7 +166,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-  origin: ['http://127.0.0.1:5173', 'http://localhost:5173'],
+  origin: ['https://jobportal-frontend-3uyu.onrender.com', 'https://jobportal-frontend-3uyu.onrender.com'],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
