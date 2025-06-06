7b657d283005ea6490aab26894897c37c907b722
Shrinath Adhav <shrinathadhav45@gmail.com>
2025-06-06 16:47:29 +0530

    changes

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -102,7 +102,7 @@ export const login = async (req, res) => {
             .cookie("token", token, {
                 maxAge: 1 * 24 * 60 * 60 * 1000,
                 httpOnly: true,
-                sameSite: 'strict',
+                sameSite: 'None',
             })
             .json({
                 message: `Welcome back ${user.fullname}`,
